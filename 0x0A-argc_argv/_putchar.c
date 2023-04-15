69ab9415778a108f672a4c94e9f1dd6481c9d008
SeifMohamed10 <seifmohamed1offical@gmail.com>
2023-04-15 11:07:55 -0700

    first 3

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c:Ther character to print
+ *
+ * Return:1 on success
+ * On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
